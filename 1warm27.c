117434bc5d311da85de372d3d0c869a18aa3b372
Saipreetha <saipreethaekb@gmail.com>
2017-04-11 15:56:17 +0530

    Add files via upload

---
+++ 1warm27.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the year:");
+scanf("%d",&a);
+if(a%400==0||(a%100!=0&&a%4==0))
+{
+printf("%d is a leap year",a);
+}
+else
+{
+printf("%d is not a leap year",a);
+}
+}
